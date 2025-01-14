680cd2e50b60df0cf105cd2fbfc7b343022140f3
Juan Pablo Cortés Mora <juanp_cm97@hotmail.com>
2025-01-14 18:49:23 -0500

    asd

--- startup.sh
+++ startup.sh
@@ -68,6 +68,7 @@ ss_output=$(ss -tulnp | grep :8000)
 # Check if output exists (i.e., if port 8000 is being used)
 if [[ -z "$ss_output" ]]; then
     send_to_discord "Port 8000 is not in use."
+    send_to_discord "Running application..."
     GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' -c /opt/startup/gunicorn.conf.py --chdir=/home/site/wwwroot" gunicorn app:app
 else
     send_to_discord "The following processes are using port 8000:\n$ss_output"
