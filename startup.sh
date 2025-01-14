bcc54add6787de4ce2873d9ffa6b43ae316fa34c
Juan Pablo Cortés Mora <juanp_cm97@hotmail.com>
2025-01-14 17:14:45 -0500

    asd

--- startup.sh
+++ startup.sh
@@ -62,5 +62,5 @@ chmod -x startup.sh
 # Start the FastAPI app with Gunicorn (capture output)
 echo "Starting FastAPI app guvicorn..."
 send_to_discord "Starting FastAPI app with python cmd..."
-python -m uvicorn main:app --host 0.0.0.0
+#python -m uvicorn main:app --host 0.0.0.0
 send_to_discord "Finished"
