d606acf2de9661a24a7113797609488b7731fe72
Juan Pablo Cortés Mora <juanp_cm97@hotmail.com>
2025-01-14 16:49:32 -0500

    geez

--- startup.sh
+++ startup.sh
@@ -15,52 +15,52 @@ send_to_discord() {
          "https://discord.com/api/webhooks/1328763919363477524/CnA6ZInh1EtZlu8oXp3kfFhjAb_uqViic8TfLNbmrjwHXPkOmkm9ZkM6JRGh7-Hc4Y2H"
 }
 
-send_to_discord "Installing apt list"
+send_to_discord "Updating apt list"
 echo -e "deb http://archive.debian.org/debian stretch main contrib non-free\ndeb http://archive.debian.org/debian-security stretch/updates main contrib non-free" | tee /etc/apt/sources.list > /dev/null
 apt-get update
+send_to_discord "Updated apt list"
 
 send_to_discord "Installing Git"
 apt-get install -y git
+send_to_discord "InstalledGit"
 
 send_to_discord "Cloning repo https://github.com/Shanksu7/fast_api_test into folder app..."
 git clone https://github.com/Shanksu7/fast_api_test app
-
+send_to_discord "Cloned repo..."
 cd app
 git pull
-
-# Send initial notification to Discord
-send_to_discord "Starting FastAPI app deployment... Pre-Installing requirements"
-
-# Get and send the current folder path to Discord
-current_folder=$(pwd)
-send_to_discord "Current folder: $current_folder"
+send_to_discord "Pulled repo"
 
 # Install pre-install requirements (capture output)
-echo "Installing pre-install requirements..."
-send_to_discord "Installing pre-install requirements..."
+send_to_discord "Pre-install requirements..."
 preinstall_output=$(pip install -r preinstall-requirements.txt 2>&1)
 send_to_discord "$preinstall_output"
+send_to_discord "Pre-install requirements installed."
 
 # Install main requirements (capture output)
 echo "Installing main requirements..."
 send_to_discord "Installing main requirements..."
 requirements_output=$(pip install -r requirements.txt 2>&1)
 send_to_discord "$requirements_output"
+send_to_discord "Installed main requirements."
 
 # Run setup script (capture output)
 echo "Running setup script..."
 send_to_discord "Running setup script..."
 setup_output=$(python setup_script.py 2>&1)
 send_to_discord "$setup_output"
+send_to_discord "setup script done."
 
 echo "Moving to /home/site/wwwroot/..."
-send_to_discord "Moving to /home/site/wwwroot/..."
+send_to_discord "Moving files to /home/site/wwwroot/..."
 mv /home/site/wwwroot/app/* /home/site/wwwroot/
 cd /home/site/wwwroot/
+send_to_discord "removing /home/site/wwwroot/app"
 rm -r app
+send_to_discord "Giving execution rights to startup.sh"
 chmod -x startup.sh
 # Start the FastAPI app with Gunicorn (capture output)
-echo "Starting FastAPI app..."
+echo "Starting FastAPI app guvicorn..."
 send_to_discord "Starting FastAPI app..."
 exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 application:app
 send_to_discord "Finished"
